90ff925406f4de79a65b9e05bd4f847a08e41a93
unknown <fkrapo@gmail.com>
2023-06-11 12:18:21 -0300

    asd

--- Fast_API.py
+++ Fast_API.py
@@ -16,87 +16,8 @@ MoviesDataset = pd.read_csv("movies_dataset_final.csv", sep=',')
 
 app = FastAPI()
 
-MoviesOverviews = []
-MoviesTitle = []
-
-for i in range(len(MoviesDataset)):
-    MoviesOverviews.append(MoviesDataset["overview"][i])
-    MoviesTitle.append(MoviesDataset["title"][i])
-
-from nltk.tokenize import word_tokenize
-
-nltk.download("punkt")
-
-for i in range(len(MoviesOverviews)):
-    if type(MoviesOverviews[i]) == str: 
-        MoviesOverviews[i] = word_tokenize(MoviesOverviews[i])
-    else:
-        MoviesOverviews[i] = []
-
-import string
-
-MoviesOverviews2 = [[] for _ in range(len(MoviesOverviews))]
-
-for i in range(len(MoviesOverviews)):
-    for word in MoviesOverviews[i]:
-        for letter in word:
-            if letter in string.punctuation:
-                word=word.replace(letter,"")
-        MoviesOverviews2[i].append(word)
-
-MoviesOverviews3 = [[] for _ in range(len(MoviesOverviews))]
-
-for i in range(len(MoviesOverviews2)):
-    for word in MoviesOverviews2[i]:
-        if word != "":
-            MoviesOverviews3[i].append(word)
-
-MoviesOverviews4 = [[] for _ in range(len(MoviesOverviews))]
-
-for i in range(len(MoviesOverviews3)):
-    for word in MoviesOverviews3[i]:
-        word = word.lower()
-        MoviesOverviews4[i].append(word)
-
-MoviesOverviews5 = [[] for _ in range(len(MoviesOverviews))]
-
-for i in range(len(MoviesOverviews4)):
-    for word in MoviesOverviews4[i]:
-        if len(word)>=3:
-            MoviesOverviews5[i].append(word)
-
-from nltk.corpus import stopwords
-
 nltk.download("stopwords")
-
-a=set(stopwords.words('english'))
-
-MoviesOverviews6 = [[] for _ in range(len(MoviesOverviews))]
-
-for i in range(len(MoviesOverviews5)):
-    MoviesOverviews6[i] = [word for word in MoviesOverviews5[i] if word not in a]
-
-
-from gensim import corpora, models, similarities
-
-
-generador_elementos = (elemento for elemento in MoviesOverviews6)
-
-
-diccionario = corpora.Dictionary(generador_elementos)
-
-
-ListaCorpus = []
-
-for i in range(len(MoviesOverviews6)):
-    ListaCorpus.append(diccionario.doc2bow(MoviesOverviews6[i]))
-
-tfidf = models.TfidfModel(ListaCorpus)
-
-corpus_tfidf = tfidf[ListaCorpus]
-
-index = similarities.MatrixSimilarity(corpus_tfidf)
-
+nltk.download("punkt")
 
 #Pantalla de inicio
 @app.get("/")
@@ -211,6 +132,85 @@ def get_director(nombre_director: str):
 @app.get('/recomendacion/{titulo}')
 def recomendacion(titulo:str):
     titulo = titulo.title()
+
+    MoviesOverviews = []
+    MoviesTitle = []
+
+    for i in range(len(MoviesDataset)):
+        MoviesOverviews.append(MoviesDataset["overview"][i])
+        MoviesTitle.append(MoviesDataset["title"][i])
+
+    from nltk.tokenize import word_tokenize
+
+    for i in range(len(MoviesOverviews)):
+        if type(MoviesOverviews[i]) == str: 
+            MoviesOverviews[i] = word_tokenize(MoviesOverviews[i])
+        else:
+            MoviesOverviews[i] = []
+
+    import string
+
+    MoviesOverviews2 = [[] for _ in range(len(MoviesOverviews))]
+
+    for i in range(len(MoviesOverviews)):
+        for word in MoviesOverviews[i]:
+            for letter in word:
+                if letter in string.punctuation:
+                    word=word.replace(letter,"")
+            MoviesOverviews2[i].append(word)
+
+    MoviesOverviews3 = [[] for _ in range(len(MoviesOverviews))]
+
+    for i in range(len(MoviesOverviews2)):
+        for word in MoviesOverviews2[i]:
+            if word != "":
+                MoviesOverviews3[i].append(word)
+
+    MoviesOverviews4 = [[] for _ in range(len(MoviesOverviews))]
+
+    for i in range(len(MoviesOverviews3)):
+        for word in MoviesOverviews3[i]:
+            word = word.lower()
+            MoviesOverviews4[i].append(word)
+
+    MoviesOverviews5 = [[] for _ in range(len(MoviesOverviews))]
+
+    for i in range(len(MoviesOverviews4)):
+        for word in MoviesOverviews4[i]:
+            if len(word)>=3:
+                MoviesOverviews5[i].append(word)
+
+    from nltk.corpus import stopwords
+
+    a=set(stopwords.words('english'))
+
+    MoviesOverviews6 = [[] for _ in range(len(MoviesOverviews))]
+
+    for i in range(len(MoviesOverviews5)):
+        MoviesOverviews6[i] = [word for word in MoviesOverviews5[i] if word not in a]
+
+
+    from gensim import corpora, models, similarities
+
+
+    generador_elementos = (elemento for elemento in MoviesOverviews6)
+
+
+    diccionario = corpora.Dictionary(generador_elementos)
+
+
+    ListaCorpus = []
+
+    for i in range(len(MoviesOverviews6)):
+        ListaCorpus.append(diccionario.doc2bow(MoviesOverviews6[i]))
+
+    tfidf = models.TfidfModel(ListaCorpus)
+
+    corpus_tfidf = tfidf[ListaCorpus]
+
+    index = similarities.MatrixSimilarity(corpus_tfidf)
+
+
     posicion = MoviesTitle.index(titulo)
 
     similitudes = index[corpus_tfidf[posicion]]
