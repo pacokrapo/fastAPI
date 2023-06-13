efc5e9c87ca8f0f46d8f1c575e2916ddaa0b435f
unknown <fkrapo@gmail.com>
2023-06-12 22:33:46 -0300

    render

--- Fast_API.py
+++ Fast_API.py
@@ -237,4 +237,4 @@ def recomendacion(titulo:str):
     for i in range(len(documentos_similares_indices)):
         Titulos.append(MoviesTitle[documentos_similares_indices[i]])
 
-    return {'lista recomendada': Titulos}
+    return {'lista recomendada': Titulos}"""
